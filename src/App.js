169a26c365c70889dbafeb610b85e05209fc9f5d
mikemazara <mike.mazara@gmail.com>
2023-06-25 11:01:43 -0700

    fix pathways

--- src/App.js
+++ src/App.js
@@ -45,7 +45,6 @@ function App() {
               />
             }
           />
-          <Route path="/related" element={<h1>About</h1>} />
           <Route path="/about" element={<h1>About</h1>} />
           <Route path="*" element={<h1>Not Found</h1>} />
         </Routes>
